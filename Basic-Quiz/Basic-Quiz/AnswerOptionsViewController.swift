41dcce2d46008db64653f0bd6f81639596f7d5ca
Pavel Palancica <pavel.palancica@gmail.com>
2019-01-04 09:49:36 +0200

    Added UI functionality to AnswerOptionsViewController for Submitting the answer and showing the Correctness

--- Basic-Quiz/Basic-Quiz/AnswerOptionsViewController.swift
+++ Basic-Quiz/Basic-Quiz/AnswerOptionsViewController.swift
@@ -13,15 +13,67 @@ import UIKit
 class AnswerOptionsViewController: UIViewController {
     
     var questionAnswerOptions: NSDictionary!
+    
+    lazy var answerOptions = questionAnswerOptions["answerOptions"] as! [String]
+    
+    private var selectedIndex: Int?
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answersTableView: UITableView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var submitAnswerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print(questionAnswerOptions)
+        
+        questionLabel.text = questionAnswerOptions["question"] as? String
+        resultLabel.text = ""
+        submitAnswerButton.isEnabled = false
     }
     
     @IBAction func onCancelButtonTap(_ sender: Any) {
         presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSubmitAnswerButtonTap(_ sender: Any) {
+        if let selectedIndex = selectedIndex {
+            let selectedAnswer = answerOptions[selectedIndex]
+            let correctAnswer = questionAnswerOptions["correctAnswer"] as! String
+            
+            if selectedAnswer == correctAnswer {
+                resultLabel.text = "CORRECT"
+            } else {
+                resultLabel.text = "INCORRECT"
+            }
+            
+            answersTableView.isUserInteractionEnabled = false
+            submitAnswerButton.isEnabled = false
+        }
+    }
+}
+
+extension AnswerOptionsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answerOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.numberOfLines = 0
+        let answer = answerOptions[indexPath.row]
+        cell.textLabel?.text = answer
+        return cell
+    }
+}
+
+extension AnswerOptionsViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        submitAnswerButton.isEnabled = true
     }
 }
